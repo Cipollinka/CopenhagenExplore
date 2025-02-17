9718c67aff56e244973cefff0fbb08a8aaa17436
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-17 15:39:09 +0200

    first commit

---
+++ src/Params.js
@@ -0,0 +1,7 @@
+export default class Params {
+  static keyPush = 'cdf1e65b-88a8-47b9-b070-6be64290f1f3'; // ключ OneSignal
+  static appID = '6741873484'; // ID додатку в сторі
+  static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
+  static targetDate = '2025-03-12'; // дата закінчення відльожки
+  static bodyLin = 'https://fantastic-prestigious-rapture.space/2vdxhm6C'; // тіло лінки з якою будемо працювати
+}
